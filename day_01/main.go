bb339532bbccc47ee90104eac0a6378667bc92f2
Benjamin Wallberg <me@bwallberg.com>
2020-12-02 12:36:47 +0100

    fix: formatting

--- day_01/main.go
+++ day_01/main.go
@@ -1,26 +1,26 @@
 package main
 
-import(
+import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func getExpenseSum(expenses []int) int {
 	for _, expense := range expenses {
 		for _, expense2 := range expenses {
-			if expense + expense2 == 2020  {
+			if expense+expense2 == 2020 {
 				return expense * expense2
 			}
 		}
-	} 
-	return -1;
+	}
+	return -1
 }
 
-func mapToInt(array []string) []int  {
-	intArray := make([]int, len(array))	
+func mapToInt(array []string) []int {
+	intArray := make([]int, len(array))
 	for _, value := range array {
 		num, err := strconv.Atoi(value)
 		if err == nil {
